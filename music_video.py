e1682d8a89e0c7f54d24b07ac2c1886a188b3425
Manuel Tiago Pereira <mt.pereira@gmail.com>
2016-07-14 11:09:48 +0100

    Add FPS to music_video.py.

--- music_video.py
+++ music_video.py
@@ -5,4 +5,4 @@ from sys import argv
 
 audio = AudioFileClip(argv[1])
 clip = ImageClip(argv[2]).set_duration(audio.duration).set_audio(audio)
-clip.write_videofile(argv[3])
+clip.write_videofile(argv[3], fps=24)
